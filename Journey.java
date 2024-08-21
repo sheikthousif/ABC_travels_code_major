5bd8d548ddf828c625c7bc9297d9ec3c58c5d637
Sheik Thouisf <sheikthousif789@gmail.com>
2024-08-21 13:14:27 +0530

    Add files via upload

---
+++ Journey.java
@@ -0,0 +1,37 @@
+package com.model;
+
+import java.time.LocalDate;
+
+public class Journey {
+	
+	 private LocalDate journeyDate;
+	    private int numberOfPassengers;
+
+	    public Journey(LocalDate journeyDate, int numberOfPassengers) {
+	        this.journeyDate = journeyDate;
+	        this.numberOfPassengers = numberOfPassengers;
+	    }
+
+		public LocalDate getJourneyDate() {
+			return journeyDate;
+		}
+
+		public void setJourneyDate(LocalDate journeyDate) {
+			this.journeyDate = journeyDate;
+		}
+
+		public int getNumberOfPassengers() {
+			return numberOfPassengers;
+		}
+
+		public void setNumberOfPassengers(int numberOfPassengers) {
+			this.numberOfPassengers = numberOfPassengers;
+		}
+
+		@Override
+		public String toString() {
+			return "Journey [journeyDate=" + journeyDate + ", numberOfPassengers=" + numberOfPassengers + "]";
+		}
+
+
+}
